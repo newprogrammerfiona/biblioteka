6ee8ad66729d6f4055886855d58e9e5b99958157
newprogrammerfiona <fjonasulanjaku1@gmail.com>
2023-05-20 14:36:16 +0200

    h

---
+++ biblioteka/src/main/java/biblioteka/org/BibliotekaApplication.java
@@ -0,0 +1,13 @@
+package biblioteka.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotekaApplication.class, args);
+	}
+
+}
